c7ca2acd3ded7d32619ac1903a938d5b71c6d698
Corey McCourt <mccourtrc@gmail.com>
2023-06-25 11:47:16 -0700

    config: try 127.0.0.1

--- src/server.rs
+++ src/server.rs
@@ -74,7 +74,7 @@ impl ChatRoom for ChatRoomService {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let port = std::env::var("PORT").expect("missing PORT env var");
-    let addr = format!("[::1]:{}", port).parse()?;
+    let addr = format!("127.0.0.1:{}", port).parse()?;
 
     println!("connected to PORT {}", port);
 
