7681be76368498861b7cebc25f923ac974b0c7e4
Corey McCourt <mccourtrc@gmail.com>
2023-06-25 11:56:28 -0700

    config: print addr

--- src/server.rs
+++ src/server.rs
@@ -74,8 +74,9 @@ impl ChatRoom for ChatRoomService {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let port = std::env::var("PORT").expect("missing PORT env var");
-    let addr = format!("127.0.0.1:{}", port).parse()?;
+    let addr = format!("0.0.0.0:{}", port).parse()?;
 
+    println!("addr {:?}", addr);
     println!("connected to PORT {}", port);
 
     let greeter = GreeterService::default();
